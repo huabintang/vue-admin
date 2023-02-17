b145857c3776d1761f82009ae139cb1e6596fb50
tang <18779714041@163.com>
2023-02-17 11:37:03 +0800

    初始化

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -7,35 +7,31 @@ const getDefaultState = () => {
   return {
     token: getToken(),
     name: '',
-    avatar: '',
-    roles:[]
+    avatar: ''
   }
 }
 
 const state = getDefaultState()
 
 const mutations = {
-  RESET_STATE: state => {
-    Object.assign(state, getDefaultState());
+  RESET_STATE: (state) => {
+    Object.assign(state, getDefaultState())
   },
   SET_TOKEN: (state, token) => {
-    state.token = token;
+    state.token = token
   },
   SET_NAME: (state, name) => {
-    state.name = name;
+    state.name = name
   },
   SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar;
-  },
-  SET_ROLES: (state, roles) => {
-    state.roles = roles;
+    state.avatar = avatar
   }
-};
+}
 
 const actions = {
   // user login
   login({ commit }, userInfo) {
-    const { username, password } = userInfo;
+    const { username, password } = userInfo
     return new Promise((resolve, reject) => {
       login({
         username: username.trim(),
@@ -50,63 +46,53 @@ const actions = {
         .catch(error => {
           reject(error);
         });
-    });
+    })
   },
 
   // get user info
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
-      getInfo(state.token)
-        .then(response => {
-          const { data } = response;
+      getInfo(state.token).then(response => {
+        const { data } = response
 
-          if (!data) {
-            reject("Verification failed, please Login again.");
-          }
+        if (!data) {
+          reject('Verification failed, please Login again.')
+        }
 
-          const { roles, name, avatar } = data;
+        const { name, avatar } = data
 
-          // roles must be a non-empty array
-          if (!roles || roles.length <= 0) {
-            reject("getInfo: roles must be a non-null array!");
-          }
-
-          commit("SET_ROLES", roles);
-          commit("SET_NAME", name);
-          commit("SET_AVATAR", avatar);
-          resolve(data);
-        })
-        .catch(error => {
-          reject(error);
-        });
-    });
+        commit('SET_NAME', name)
+        commit('SET_AVATAR', avatar)
+        resolve(data)
+      }).catch(error => {
+        reject(error)
+      })
+    })
   },
 
   // user logout
   logout({ commit, state }) {
     return new Promise((resolve, reject) => {
-      logout(state.token)
-        .then(() => {
-          removeToken(); // must remove  token  first
-          resetRouter();
-          commit("RESET_STATE");
-          resolve();
-        })
-        .catch(error => {
-          reject(error);
-        });
-    });
+      logout(state.token).then(() => {
+        removeToken() // must remove  token  first
+        resetRouter()
+        commit('RESET_STATE')
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
   },
 
   // remove token
   resetToken({ commit }) {
     return new Promise(resolve => {
-      removeToken(); // must remove  token  first
-      commit("RESET_STATE");
-      resolve();
-    });
+      removeToken() // must remove  token  first
+      commit('RESET_STATE')
+      resolve()
+    })
   }
-};
+}
 
 export default {
   namespaced: true,
